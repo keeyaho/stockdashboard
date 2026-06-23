38950adf47c5d1d12fb55462bf464b3d5dbd1a1d
keeyaho <84953126+keeyaho@users.noreply.github.com>
2026-06-23 15:14:38 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -492,7 +492,7 @@ st.components.v1.html(
 
     from fredapi import Fred
 
-fred = Fred(api_key="네API키")
+fred = Fred(api_key="300bba1708243c908ec9474dc9b3c0eb")
 
 search = fred.search("Eastern Gas South")
 print(search.head(20))
